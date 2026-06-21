104f2d7fec98d3e2ede546c8292c4135c2ad9d09
ling-lan2 <1428030@qq.com>
2026-06-21 22:51:00 +0800

    忘记了

--- qt/jjm.h
+++ qt/jjm.h
@@ -7,16 +7,35 @@
 #include <QCryptographicHash>
 #include <QDir>
 
-
 namespace MachineKey {
-
 static QByteArray generateKey(const QString &appid) {
-    return QByteArray();
+    QString machineInfo;
+    machineInfo += QHostInfo::localHostName();
+    machineInfo += QSysInfo::kernelType();
+    machineInfo += QSysInfo::currentCpuArchitecture();
+    machineInfo += QDir::home().absolutePath();
+    machineInfo += appid;
+    return QCryptographicHash::hash(machineInfo.toUtf8(), QCryptographicHash::Sha256);
 }
 
 
 static QString encryptDecrypt(const QString &text, const QByteArray &key, bool doEncrypt) {
-    return text;
+    QByteArray data;
+    if (doEncrypt) {
+        data = text.toUtf8();
+    } else {
+        data = QByteArray::fromBase64(text.toLatin1());
+    }
+
+    for (int i = 0; i < data.size(); ++i) {
+        data[i] = data[i] ^ key[i % key.size()];
+    }
+
+    if (doEncrypt) {
+        return QString::fromLatin1(data.toBase64());
+    } else {
+        return QString::fromUtf8(data);
+    }
 }
 
 inline QString encrypt(const QString &plain, const QByteArray &key) {
